cbd9bfc68b8334167616d3a0b696cf732fa6ec6a
Anastasia <nastya-shishkina-1999@mail.ru>
2022-11-19 20:41:40 +0300

    task 41

---
+++ task41/Program.cs
@@ -0,0 +1,29 @@
+﻿/// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите числа через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine($"Количество чисел больше 0 = {count}");
